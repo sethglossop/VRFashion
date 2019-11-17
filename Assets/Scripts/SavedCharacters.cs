da91c8ec7059514ab1065ae44329b28b0633ba5d
chantheboy <56391666+chantheboy@users.noreply.github.com>
2019-11-17 16:36:38 +0000

    CharacterSaveStart

---
+++ Assets/Scripts/SavedCharacters.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UMA;
+using UMA.CharacterSystem;
+
+public class SavedCharacters : MonoBehaviour
+{
+    private DynamicCharacterAvatar character;
+    public string savedCharacter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<DynamicCharacterAvatar>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SaveCharacter()
+    {
+        savedCharacter = character.GetCurrentRecipe();
+    }
+
+    public void LoadCharacter()
+    {
+        character.ClearSlots();
+        character.LoadFromRecipeString(savedCharacter);
+    }
+}
