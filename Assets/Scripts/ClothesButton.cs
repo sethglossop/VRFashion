36f2fc81e0de8985754fa221769289672d1b93f3
chantheboy <56391666+chantheboy@users.noreply.github.com>
2020-01-20 18:26:20 +0000

    ItemDescriptionPanel

---
+++ Assets/Scripts/ClothesButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClothesButton : MonoBehaviour
+{
+    public string itemDescription;
+    public string itemPrice;
+    public Sprite itemPicture;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        itemPicture = GetComponent<Image>().sprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PopUpItem(PopUpPanel popUpPanel)
+    {
+        popUpPanel.panelDescription.text = itemDescription;
+        popUpPanel.panelPicture.sprite = itemPicture;
+        popUpPanel.panelPrice.text = itemPrice;
+    }
+}
---
+++ Assets/Scripts/PopUpPanel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopUpPanel : MonoBehaviour
+{
+    public Text panelDescription;
+    public Text panelPrice;
+    public Image panelPicture;
+
+    //   public Sprite picture;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //public void ItemPicture(Sprite buttonPicture)
+    //{
+    //    newPicture.sprite = buttonPicture;
+    //}
+
+    //public void ItemDescription (string buttonDescription)
+    //{
+    //    newDesc.text = buttonDescription;
+    //}
+
+}
+
